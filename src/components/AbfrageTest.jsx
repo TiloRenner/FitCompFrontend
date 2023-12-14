f4f2bd116b8acb82fb0bd5f166aa5e40fc99c1b3
Tilo <info@tilorenner.com>
2023-12-14 14:23:14 +0100

    simple array rendered

--- src/components/AbfrageTest.jsx
+++ src/components/AbfrageTest.jsx
@@ -3,15 +3,34 @@
 
 function AbfrageTest({abfrage}){
 
-    return <h2>Hallo , hier die Abfrage</h2>
+    let displayCont = buildPage([1,1,1,2])
+    console.log("Cont:", displayCont)
+
+    return <h2>{displayCont}</h2>
 
 }
 
 
 
-function buildPage()
+function buildPage(array)
 {
-    
+
+    console.log("Array", array)
+    const result = array.map((element) => {
+        if(element == 1)
+        {
+            return <p> Paragraph1</p>
+        }
+        else
+        {
+            return <button>Button</button>
+        }
+
+    }
+
+    )
+
+    return result;
 }
 
 export default AbfrageTest;
